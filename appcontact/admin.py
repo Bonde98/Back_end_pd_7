9b62bc61c97df35d962f81dd7c67c858e2f61f6e
Bonde98 <badianeasse99@gmail.com>
2022-03-10 18:51:40 +0100

    jjkjk

--- appcontact/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Tabcontact
-admin.site.register(Tabcontact)
--- appcontact/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppcontactConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appcontact'
