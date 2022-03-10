6d3e8a772265db20442e29705a9b4b7a644b9d27
Bonde98 <badianeasse99@gmail.com>
2022-03-10 17:51:55 +0100

    Initial commit

--- blogbackend/settings.py
+++ blogbackend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
-import dj_database_url
+from django import dj_database_url
 from pathlib import Path
 from telnetlib import LOGOUT
 #from re import template
@@ -148,7 +148,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 		
-'''STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+'''
 
 PROJET_ROOT = BASE_DIR
 STATIC_ROOT = os.path.join(PROJET_ROOT, 'staticfiles')
