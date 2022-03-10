98b4e1c45991bc1a95c7b375426363af476492b7
Bonde98 <badianeasse99@gmail.com>
2022-03-10 16:44:59 +0100

    Initial commi

--- blogbackend/settings.py
+++ blogbackend/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'	
 
-from django import dj_database_url
+import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 		
