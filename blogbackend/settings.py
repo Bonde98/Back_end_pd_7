d2c701a55a5812200330eafac9fbe2b5c5538e23
Bonde98 <badianeasse99@gmail.com>
2022-03-10 18:38:24 +0100

    Initial commit

--- blogbackend/settings.py
+++ blogbackend/settings.py
@@ -146,11 +146,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'	
 
 
-
+'''
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES = ['default'].update(prod_db)
 		
-'''
+
 
 PROJET_ROOT = BASE_DIR
 STATIC_ROOT = os.path.join(PROJET_ROOT, 'staticfiles')
