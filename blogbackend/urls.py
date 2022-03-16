3444e6f9185a3c352cc5e4d964e645876408a6a3
Bonde98 <badianeasse99@gmail.com>
2022-03-16 12:23:05 +0100

    mettre a jour

--- blogbackend/urls.py
+++ blogbackend/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('',include("appcontact.urls")),
     path('',include("myarticle.urls")),
+   
 
     
 ]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
